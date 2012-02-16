1e7e5a0a66d1ba06265e93a4218b3b97a286c69f
Mark Drew <mark.drew@gmail.com>
2012-02-16 11:53:25 +0000

    Moved tests

--- org.cfeclipse.cfmledit.tests/src/org/cfeclipse/cfmledit/tests/dictionary/VersionTest.java
+++ org.cfeclipse.cfmledit.tests/src/org/cfeclipse/cfmledit/tests/dictionary/VersionTest.java
@@ -2,18 +2,20 @@ package org.cfeclipse.cfmledit.tests.dictionary;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.cfeclipse.cfmledit.dictionary.Dictionary;
+import org.cfeclipse.cfmledit.dictionary.Version;
 import org.junit.Test;
 
 public class VersionTest {
 
 	@Test
-	public void testGetTags() {
-		fail("Not yet implemented");
+	public void testGetVersionFromDictionary() throws Exception {
+		Dictionary dict = Dictionary.getInstance();
+		ArrayList<Version> versions = dict.getVersions();
+		assertEquals("org.cfeclipse.cfmledit.dictionary.Version", versions.get(0).getClass().getName());
 	}
 
-	@Test
-	public void testGetFunctions() {
-		fail("Not yet implemented");
-	}
 
 }
